1cd1033e1c06d61026159db2850a8dc166ab80d5
Kevin Kirn <kevin.kirn@students.fhnw.ch>
2018-01-22 16:04:08 +0100

    defines custom table names for jpa entities

--- week4/src/main/java/ch/fhnw/edu/rental/model/PriceCategory.java
+++ week4/src/main/java/ch/fhnw/edu/rental/model/PriceCategory.java
@@ -5,18 +5,20 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "PRICECATEGORIES")
 @DiscriminatorColumn(name = "PRICECATEGORY_TYPE")
 public abstract class PriceCategory {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	public PriceCategory() {
 	}
-	
+
 	public Long getId() {
 		return id;
 	}
