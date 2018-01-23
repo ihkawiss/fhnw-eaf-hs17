9125e3f8dca4cbb8a13c3f5479b9cf086419cd25
Kevin Kirn <kevin.kirn@students.fhnw.ch>
2018-01-23 09:52:41 +0100

    fixes build since wrong relationship was mapped

--- week4/src/main/java/ch/fhnw/edu/rental/model/Rental.java
+++ week4/src/main/java/ch/fhnw/edu/rental/model/Rental.java
@@ -9,8 +9,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -24,7 +24,7 @@ public class Rental {
 	@Column(name = "RENTAL_ID")
 	private Long id;
 
-	@ManyToMany
+	@OneToOne
 	@JoinColumn(name = "MOVIE_ID")
 	private Movie movie;
 	
