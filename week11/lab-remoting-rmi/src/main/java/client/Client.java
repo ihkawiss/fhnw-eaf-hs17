dc95edf649e0fd05d4adaca9b9a81e5c56603736
Kevin Kirn <kevin.kirn@students.fhnw.ch>
2018-02-03 16:05:19 +0100

    adds rmi lab solutions

--- week11/lab-remoting-rmi/src/main/java/client/Client.java
+++ week11/lab-remoting-rmi/src/main/java/client/Client.java
@@ -1,5 +1,8 @@
 package client;
 
+import java.rmi.Naming;
+import java.rmi.Remote;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -7,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.remoting.rmi.RmiProxyFactoryBean;
 
+import bank.Account;
 import bank.AccountService;
 
 @SpringBootApplication
@@ -36,6 +40,27 @@ public class Client {
 			for (Class<?> c : proxy.getClass().getInterfaces()) {
 				System.out.println("> " + c.getCanonicalName());
 			}
+			
+			// Aufgabe 2
+			for(int i = 0; i < 10; i++) {
+				service.insertAccount(new Account("Private"));
+			}
+		
+			if(!service.getAccounts("Private").isEmpty()) {
+				if(service.getAccounts("Private").size() % 10 == 0)
+					System.out.println("Account was created via RMI!");
+				else
+					System.err.println("Count of created accounts wrong!" + service.getAccounts("Private").size());
+			} else {
+				System.err.print("RMI creation failed!");
+			}
+			
+			
+			// Aufgabe 3
+			proxy = Naming.lookup("rmi://localhost:1099/AccountService");
+			System.out.println("proxy.getClass() = " + proxy.getClass().getCanonicalName());
+			System.out.println(String.format("Castable to AccountService?  %s!", proxy instanceof AccountService)); 			
+			
 		};
 	}
 
