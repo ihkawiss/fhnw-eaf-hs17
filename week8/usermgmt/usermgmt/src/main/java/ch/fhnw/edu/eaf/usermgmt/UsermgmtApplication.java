f94a5b44857fd83c95af0f558a8e1a2084fc60ff
Kevin Kirn <kevin.kirn@students.fhnw.ch>
2018-02-03 09:43:26 +0100

    adds spring boot project of week 8

---
+++ week8/usermgmt/usermgmt/src/main/java/ch/fhnw/edu/eaf/usermgmt/UsermgmtApplication.java
@@ -0,0 +1,12 @@
+package ch.fhnw.edu.eaf.usermgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsermgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsermgmtApplication.class, args);
+	}
+}
